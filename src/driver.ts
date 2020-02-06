67278d987fe03d9001eeb307900ddce65959c6a6
Andrew Jo <andrew@verdigris.co>
2020-02-06 11:01:04 -0800

    Driver abstract class

---
+++ src/driver.ts
@@ -0,0 +1,52 @@
+/**
+ * Abstract base class required for implementing a cache driver.
+ */
+export abstract class Driver {
+  /**
+   * Lists all keys.
+   */
+  public abstract async keys(): Promise<string[]>;
+
+  /**
+   * Gets whether the key exists in cache.
+   * @param key {string}
+   */
+  public abstract async has(key: string): Promise<boolean>;
+
+  /**
+   * Gets the cached value at key.
+   * @param key {string}
+   */
+  public abstract async get(key: string): Promise<any>;
+
+  /**
+   * Sets a value to be cached at key.
+   * @param key {string}
+   * @param value {any}
+   * @param ttl {number} time-to-live in seconds
+   */
+  public abstract async set(
+    key: string,
+    value: any,
+    ttl?: number
+  ): Promise<boolean>;
+
+  /**
+   * Gets the time-to-live of the key in seconds.
+   * @param key {string}
+   */
+  public abstract async ttl(key: string): Promise<number>;
+
+  /**
+   * Sets the expiry duration (in seconds) for a given key.
+   * @param key {string}
+   * @param ttl {number} time-to-live in seconds
+   */
+  public abstract async expire(key: string, ttl: number): Promise<boolean>;
+
+  /**
+   * Delete a key.
+   * @param key {string}
+   */
+  public abstract async delete(keys: string | string[]): Promise<number>;
+}
