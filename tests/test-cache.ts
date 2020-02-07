868e078e115b51aef901d8cf499fe477d35f22c8
Andrew Jo <andrew@verdigris.co>
2020-02-06 22:32:12 -0800

    Add test for Cache class

---
+++ tests/test-cache.ts
@@ -0,0 +1,12 @@
+import "mocha";
+import { expect } from "chai";
+
+import Cache from "@nitrous/cache";
+import { Memory } from "@nitrous/drivers";
+
+describe("Cache", function() {
+  it("should default to Memory driver when driver is not specified in constructor", async function() {
+    const cache = new Cache();
+    expect(cache["driver"]).to.be.an.instanceof(Memory);
+  });
+});
