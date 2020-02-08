6acaabc8391eeee7b49641dab5869f0572a2a76b
Andrew Jo <andrew@verdigris.co>
2020-02-07 19:37:47 -0800

    Add TSDocs

--- src/cache.ts
+++ src/cache.ts
@@ -4,38 +4,76 @@ import { Memory } from "./drivers/memory";
 export default class Cache {
   protected driver: Driver;
 
+  /**
+   * Instantiate a new cache object.
+   * @param {Driver} driver - an instance of cache driver
+   */
   public constructor(driver?: Driver) {
     this.driver = driver ?? new Memory();
   }
 
+  /**
+   * Retrieve all keys.
+   */
   public async keys(): Promise<string[]> {
     return await this.driver.keys();
   }
 
+  /**
+   * Get whether the key exists in cache.
+   * @param {string} key - key name
+   */
   public async has(key: string): Promise<boolean> {
     return await this.driver.has(key);
   }
 
+  /**
+   * Get cached value at given key.
+   * @param {string} key - key name
+   */
   public async get(key: string): Promise<any> {
     return await this.driver.get(key);
   }
 
+  /**
+   * Set value to cache at given key.
+   * @param {string} key - key name
+   * @param {any} value - value to cache
+   * @param {number} [ttl] - key lifetime in seconds
+   */
   public async set(key: string, value: any, ttl?: number): Promise<boolean> {
     return await this.driver.set(key, value, ttl);
   }
 
+  /**
+   * Get remaining expiry time in seconds.
+   * @param {string} key - key name
+   */
   public async ttl(key: string): Promise<number> {
     return await this.driver.ttl(key);
   }
 
+  /**
+   * Update key with new expiry time in seconds.
+   * @param {string} key - key name
+   * @param {number} ttl - key lifetime in seconds
+   */
   public async expire(key: string, ttl: number): Promise<boolean> {
     return await this.driver.expire(key, ttl);
   }
 
+  /**
+   * Delete one or more keys.
+   * @param {(string|string[])} keys - key name
+   */
   public async delete(keys: string | string[]): Promise<number> {
     return await this.driver.delete(keys);
   }
 
+  /**
+   * Closes underlying driver. If external cache drivers (e.g. [[Redis]]) are used, this will close
+   * the connection. Use this method to clean up and teardown.
+   */
   public async close(): Promise<boolean> {
     return await this.driver.close();
   }
