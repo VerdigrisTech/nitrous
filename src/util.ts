42ef2191830f7f57bd50a0372807e6827665e2d3
Andrew Jo <andrew@verdigris.co>
2020-02-07 17:13:10 -0800

    Implement flatten function

---
+++ src/util.ts
@@ -0,0 +1,14 @@
+type List<T> = ArrayLike<T>;
+type Many<T> = T | ReadonlyArray<T>;
+
+export function flatten<T>(array: List<Many<T>>, result: T[] = []): T[] {
+  for (let i = 0, length = array.length; i < length; i++) {
+    const value = array[i];
+    if (Array.isArray(value)) {
+      flatten(value, result);
+    } else {
+      result.push(value as T);
+    }
+  }
+  return result;
+}
