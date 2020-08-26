176a7212454a9f4fb13ed707fce9a8ec368a5bea
Andrew Jo <andrew@verdigris.co>
2020-08-26 11:54:51 -0700

    Use TypeScript 3.8 re-export syntax

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,4 @@
 
 export { default as Cache } from "./cache";
 export { default as Driver } from "./driver";
-import * as drivers from "./drivers";
-export { drivers };
+export * as drivers from "./drivers";
